270e4efab5a349f9b967ba2aaa2b67e9b66044ba
Sergey <shkaev.ss@mail.ru>
2023-05-02 19:20:57 +0300

    созданы балванки MVP, сделан класс Menu

---
+++ task2_2/src/base/Menu.java
@@ -0,0 +1,13 @@
+package base;
+
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList menu;
+    String name;
+
+    public Menu(ArrayList menu, String name) {
+        this.menu = menu;
+        this.name = name;
+    }
+}
